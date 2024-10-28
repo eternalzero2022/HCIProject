429a9a82bcb5936c1b46ba10f0c0f528ee8b8719
114404517@qq.com <114404517@qq.com>
2024-10-28 23:37:50 +0800

    fix: 修改了跨域访问的部分，允许前端使用cookie

--- src/main/java/com/njuse/battlerankbackend/configure/CorsFilter.java
+++ src/main/java/com/njuse/battlerankbackend/configure/CorsFilter.java
@@ -19,9 +19,10 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, token");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "3600");
         if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())){
             response.setStatus(200);
