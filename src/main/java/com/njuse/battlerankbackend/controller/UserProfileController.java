ff2d428b85d2d167d570790bdd4211ba2b49b834
sprooc <1127626033@qq.com>
2024-12-15 19:27:39 +0800

    feature: 添加了根据userId查看用户创建、收藏、参与的合集的接口

--- src/main/java/com/njuse/battlerankbackend/controller/UserProfileController.java
+++ src/main/java/com/njuse/battlerankbackend/controller/UserProfileController.java
@@ -7,6 +7,7 @@ import com.njuse.battlerankbackend.vo.CollectionVO;
 import com.njuse.battlerankbackend.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +36,20 @@ public class UserProfileController {
     @GetMapping("/favorites")
     public ResultVO<List<CollectionVO>> getFavoriteCollections() {
         return ResultVO.buildSuccess(userProfileService.getFavouriteCollections(securityUtil.getCurrentUser().getUserId()));
+    }
+
+    @GetMapping("/{userId}/created")
+    public ResultVO<List<CollectionVO>> getCreatedCollection(@PathVariable Integer userId) {
+        return ResultVO.buildSuccess(userProfileService.getCreatedCollections(userId));
+    }
+
+    @GetMapping("/{userId}/voted")
+    public ResultVO<List<CollectionVO>> getVotedCollection(@PathVariable Integer userId) {
+        return ResultVO.buildSuccess(userProfileService.getVotedCollections(userId));
+    }
+
+    @GetMapping("/{userId}/favorites")
+    public ResultVO<List<CollectionVO>> getFavoriteCollection(@PathVariable Integer userId) {
+        return ResultVO.buildSuccess(userProfileService.getFavouriteCollections(userId));
     }
 }
